6883247b411833d6c9fd8adfea6f9f94fcb57a56
Yves Schelpe <yves.schelpe@gmail.com>
2017-06-20 20:13:14 +0200

    Basis html opzet

--- Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/app.module.ts
+++ Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { RootComponent } from './shared/root/root.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    RootComponent
+    RootComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule    
   ],
   providers: [],
   bootstrap: [RootComponent]
