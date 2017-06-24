2b4beaf064530b3e4f0311c1c73faff561225139
Yves Schelpe <yves.schelpe@gmail.com>
2017-06-24 17:10:26 +0200

    layout in it's own module

--- Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/app.module.ts
+++ Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/app.module.ts
@@ -2,19 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { RootComponent } from './shared/root/root.component';
-import { HeaderComponent } from './shared/layout/header/header.component';
-import { FooterComponent } from './shared/layout/footer/footer.component';
 
+import { LayoutModule } from './shared/layout/layout.module';
 import { VacaturesModule } from './vacatures/vacatures.module';
 
 @NgModule({
   declarations: [
-    RootComponent,
-    HeaderComponent,
-    FooterComponent
+    RootComponent
   ],
   imports: [
     BrowserModule,
+    LayoutModule,
     VacaturesModule
   ],
   bootstrap: [
