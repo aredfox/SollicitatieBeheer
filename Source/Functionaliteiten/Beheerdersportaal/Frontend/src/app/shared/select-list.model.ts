4aa07b1e22686cdfbad400e534e33322f6462a49
Yves Schelpe <yves.schelpe@gmail.com>
2017-06-24 17:39:33 +0200

    refactored selection lists to it's own class/model for reuse

---
+++ Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/shared/select-list.model.ts
@@ -0,0 +1,26 @@
+export class SelectList<T> {
+    private constructor(
+        public items: string[],
+        public selectedItem: string)
+    { }
+
+    public static createSelectList<T>(array: T[], propertySelector: (x: T) => string, selectedItem: string = '*'): SelectList<T> {
+        const items = this.createSelectListItems(array, propertySelector);
+        let selectList = new SelectList<T>(items, selectedItem);
+        return selectList;
+    }
+
+    private static createSelectListItems<T, TResult>(array: T[], propertySelector: (x: T) => TResult | string): TResult[] {
+        return this.distinct(array
+            .map(propertySelector)
+            .concat('*'))
+            .sort();
+    }
+
+    private static distinct(array: any[]): any[] {
+        return array.reduce((p, c) => {
+            if (p.indexOf(c) < 0) p.push(c);
+            return p;
+        }, []);
+    };
+}
--- Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/vacatures/vacatures.component.ts
+++ Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/vacatures/vacatures.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Vacature } from './vacature.model';
+import { SelectList } from '../shared/select-list.model';
 
 @Component({
   selector: 'app-vacatures',
@@ -10,10 +11,8 @@ export class VacaturesComponent {
   vacatures: Vacature[];
 
   filteredVacatures: Vacature[];
-  afdelingen: string[];
-  selectedAfdeling: string;
-  functies: string[];
-  selectedFunctie: string;
+  afdelingen: SelectList<string>;
+  functies: SelectList<string>;
 
   constructor() {
     this.vacatures = [
@@ -23,10 +22,8 @@ export class VacaturesComponent {
       { nummer: 15520, functie: 'Ombuds', afdeling: 'Personeelsdienst', omschrijving: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.' },
       { nummer: 15584, functie: 'Tuinman', afdeling: 'Technische Dienst', omschrijving: '' },
     ];
-    this.afdelingen = this.createSelectList(this.vacatures, v => v.afdeling);
-    this.selectedAfdeling = '*';
-    this.functies = this.createSelectList(this.vacatures, v => v.functie);
-    this.selectedFunctie = '*';
+    this.afdelingen = SelectList.createSelectList(this.vacatures, v => v.afdeling);
+    this.functies = SelectList.createSelectList(this.vacatures, v => v.functie);
 
     this.filteredVacatures = this.vacatures;
   }
@@ -34,21 +31,7 @@ export class VacaturesComponent {
   onFilter() {
     this.filteredVacatures =
       this.vacatures
-        .filter(v => this.selectedAfdeling === '*' || v.afdeling === this.selectedAfdeling)
-        .filter(v => this.selectedFunctie === '*' || v.functie === this.selectedFunctie);
+        .filter(v => this.afdelingen.selectedItem === '*' || v.afdeling === this.afdelingen.selectedItem)
+        .filter(v => this.functies.selectedItem === '*' || v.functie === this.functies.selectedItem);
   }
-
-  private createSelectList<T, TResult>(array: T[], propertySelector: (x: T) => TResult | string): TResult[] {
-    return this.distinct(array
-      .map(propertySelector)
-      .concat('*'))
-      .sort();
-  }
-
-  private distinct(array: any[]): any[] {
-    return array.reduce((p, c) => {
-      if (p.indexOf(c) < 0) p.push(c);
-      return p;
-    }, []);
-  };
 }
