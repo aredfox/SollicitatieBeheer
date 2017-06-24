ecda41023523a584ac71b02202e2d6cfa6d42229
Yves Schelpe <yves.schelpe@gmail.com>
2017-06-24 11:17:53 +0200

    styling vacaturelijst

--- Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/shared/root/root.component.ts
+++ Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/shared/root/root.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,17 +6,4 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./root.component.less']
 })
 export class RootComponent {
-  vacatures: any[];
-  columns: any[];
-  vacatureFuncties: string[];
-
-  ngOnInit() {
-    this.vacatures = [
-      { nummer: 15486, functie: 'Begeleider', afdeling: 'OOOC Potgieter' },
-      { nummer: 15492, functie: 'Begeleider', afdeling: 'OOOC Jacob Jordaens' },
-      { nummer: 15512, functie: 'Klusjesman', afdeling: 'Technische Dienst' },
-      { nummer: 15520, functie: 'Ombuds', afdeling: 'Personeelsdienst' }
-    ];
-    this.vacatureFuncties = this.vacatures.map(v => v.functie);
-  }
 }
--- Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/vacatures/vacatures.component.ts
+++ Source/Functionaliteiten/Beheerdersportaal/Frontend/src/app/vacatures/vacatures.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-vacatures',
   templateUrl: './vacatures.component.html',
   styleUrls: ['./vacatures.component.less']
 })
-export class VacaturesComponent implements OnInit {
+export class VacaturesComponent {
+  vacatures: any[];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.vacatures = [
+      { nummer: 15486, functie: 'Begeleider', afdeling: 'OOOC Potgieter', omschrijving: '' },
+      { nummer: 15492, functie: 'Begeleider', afdeling: 'OOOC Jacob Jordaens', omschrijving: 'Et harum quidem rerum facilis est et expedita distinctio.' },
+      { nummer: 15512, functie: 'Klusjesman', afdeling: 'Technische Dienst', omschrijving: '' },
+      { nummer: 15520, functie: 'Ombuds', afdeling: 'Personeelsdienst', omschrijving: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident.' }
+    ];
   }
 
 }
